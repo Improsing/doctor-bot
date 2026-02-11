f5503b2e52b884cd993ed72fe4875a762a36ee5c
Muslim <bankurovmuslim@yandex.ru>
2026-02-11 11:42:21 +0300

    test

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	token := os.Getenv("BOT_TOKEN")
+
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	log.Printf("Бот запущен: %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil {
+			msg := tgbotapi.NewMessage(
+				update.Message.Chat.ID,
+				"Привет, я Помощник врача 👨‍⚕️",
+			)
+			bot.Send(msg)
+		}
+	}
+}
+
